83ab9b31caf4b35b4777ac2215f90e50c0663419
Michael Hadley <michaelwesthadley@gmail.com>
2018-03-06 12:43:51 -0600

    Update installer script

--- install_addons.sh
+++ install_addons.sh
@@ -3,3 +3,6 @@
 cd ../../../addons/
 git clone https://github.com/braitsch/ofxDatGui.git
 git clone https://github.com/armadillu/ofxAnimatable.git
+
+# Grab the version of ofxOMXPlayer that is compatible with OF 0.9
+git clone --branch 0.9.0-compatible https://github.com/jvcleave/ofxOMXPlayer.git
