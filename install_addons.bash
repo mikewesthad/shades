940ae1cc4a062d7629fdbd30a362dd022864109c
Michael Hadley <michaelwesthadley@gmail.com>
2018-03-06 19:41:44 -0600

    Update dependency install script

--- install_addons.bash
+++ install_addons.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
-
 cd ../../../addons/
 git clone https://github.com/braitsch/ofxDatGui.git
 git clone https://github.com/armadillu/ofxAnimatable.git
 
+# For previous pi version:
 # Grab the version of ofxOMXPlayer that is compatible with OF 0.9
-git clone --branch 0.9.0-compatible https://github.com/jvcleave/ofxOMXPlayer.git
+# git clone --branch 0.9.0-compatible https://github.com/jvcleave/ofxOMXPlayer.git
